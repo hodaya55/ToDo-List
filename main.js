d11117de3b1e56659db53c0306c6ce8be2f909fb
hodaya55 <hodayah159@gmail.com>
2018-03-29 21:18:36 +0300

    add some changes

--- main.js
+++ main.js
@@ -2,51 +2,51 @@ var list = document.getElementById('demo');
 var lastid = 0;
 
 function changeText2() {
-    var todo = document.getElementById('todo').value;
-    if(todo!="")
-    {
-        document.getElementById('boldStuff2').innerHTML = todo;
+    var todo = document.getElementById('todo');
+    if (todo.value != "") {
+        document.getElementById('boldStuff2').innerHTML = todo.value;
 
         var d = document.getElementById('date');
         d.innerHTML = Date().slice(0, 21);
-    
-    
-        //adding list item of "todo" into unorder-list
+
+        //adding list item into unorder-list
         var entry = document.createElement('li');
-        entry.appendChild(document.createTextNode(todo));
-        entry.setAttribute('id', 'item' + lastid);
-    
+
         //adding checkbox near the list item
         var check = document.createElement('input');
         check.setAttribute("type", "checkbox");
         check.setAttribute('id', 'item' + lastid);
-    
+
         // adding remove button near the list item
         var remove = document.createElement('button');
         remove.appendChild(document.createTextNode("remove"));
         remove.setAttribute('onClick', 'removeName("' + 'item' + lastid + '")');
-    
-    
+
+
+        entry.appendChild(check);
+        entry.appendChild(remove);
+        entry.appendChild(document.createTextNode(" Created on: " + Date().slice(0, 21) + "  "));
+
+
+        entry.appendChild(document.createTextNode("ToDo: " + todo.value));
+        entry.setAttribute('id', 'item' + lastid);
+
+        $('#item' + lastid + '').css('color', 'red');
+
         // button of removing all the checked items
         var all = document.getElementById('all');
         all.setAttribute('onClick', 'removeAllDone()');
-    
-    
-        entry.appendChild(check);
-        entry.appendChild(remove);
-        entry.appendChild(document.createTextNode(" Created on: " + Date().slice(0, 21)));
-    
+
         lastid += 1;
         list.appendChild(entry);
-    
+
     }
-else
-{
-    var text=document.getElementById('boldStuff2');
-    text.style.color="red";
-    document.getElementById('boldStuff2').innerHTML="Please enter what you need to do.";
-}
-   
+    else {
+        var text = document.getElementById('boldStuff2');
+        text.style.color = "red";
+        document.getElementById('boldStuff2').innerHTML = "Please enter what you need to do.";
+    }
+
 
 }
 
